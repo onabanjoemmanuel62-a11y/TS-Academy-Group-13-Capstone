c00eb3a9fb0f3cd628f22d434d02528704586add
Emmanuel <onabanjoemmanuel62@gmail.com>
2026-05-06 18:36:54 +0100

    components update

---
+++ src/components/PlanetList.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const ComponentName = () => {
+  return (
+    <div>
+      {/* Work goes here */}
+      <h1>Component Placeholder</h1>
+    </div>
+  );
+};
+
+export default PlanetList;
