02d3680af241056a16f7f9b0d9eec5d80da9608f
miracleosazuwa6-glitch <miracleosazuwa6@gmail.com>
2026-05-08 20:00:01 +0100

    moved styles to new file and added group link

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,50 +1,33 @@
 import React from 'react';
+import './Footer.css'; // Import the CSS file for styling
 
 
 const Footer = () => {
 return (
-<footer style={{
-backgroundColor: '#0a192f', 
-color: 'white',
-padding: '40px 20px',
-fontFamily: 'sans-serif',
-fontWeight: 'bold',
-}}>
-<div style={{ maxWidth: '100%', margin: '0' }}>
-
-<div style={{ textAlign: 'left', marginBottom: '20px' }}>
-<h4 style={{ marginBottom: '10px' }}>About</h4>
-
-    {/*First names of group members here*/}
-<p style={{ fontSize: '14px', }}>
+<footer className="main-footer">
+<div className="group-members">
+<h4>About</h4>
+{/*First names of group members here*/}
+<p>
     Miracle, Emmanuel, Olalekan, Olatomiwa, Edikan, Chidera, Temitope
 </p>
 </div>
 
-<hr style={{ border: '0', borderTop: '1px solid #334155', margin: '20px 0'  }} />
+<hr className="footer-hr"/>
 
-<div style={{
-display: 'flex',
-justifyContent: 'space-between',
-alignItems: 'center',
-textAlign: 'left',
-fontSize: '12px',
-fontWeight: 'bold'
-
-}}>
+<div className="footer-container">
+    
 <div>
     
-<p>©2026 Design by Amaka & Ifeoma A.</p>
+<p>©2026 Design by Novara Software Development Capstone Project Group -13</p>
 {/*Link to the GitHub repository of the project*/}
-<a href="https://github.com/onabanjoemmanuel62-a11y/TS-Academy-Group-13-Capstone" target="_blank" rel="noopener noreferrer" style={{ color: 'white', textDecoration: 'none' }}> Built by Novara Software Development Capstone project group - 13. All rights reserved</a>
+<a href="https://github.com/onabanjoemmanuel62-a11y/TS-Academy-Group-13-Capstone" target="_blank" rel="noopener noreferrer" className="footer-link"> Built by Novara Software Development Capstone project group - 13. All rights reserved</a>
 
 </div>
 
 <div>
-<strong>TSAcademy</strong>
+<a href="https://tsacademyonline.com/" target="_blank" rel="noopener noreferrer" className="footer-link">TSAcademy</a>
 </div>
-</div>
-
 </div>
 </footer>
 );
