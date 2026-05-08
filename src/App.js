1bdfc70ae670fa4a140762dddad8e268fa02058f
Emmanuel <onabanjoemmanuel62@gmail.com>
2026-05-08 10:37:16 +0100

    Update README with current project status and team info

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import React from 'react';
+import Header from './components/Header';
+import VideoSection from './components/VideoSection';
 import ContactForm from './components/ContactForm';
 import './App.css';
 
 function App() {
   return (
     <div>
+      <Header />
+      <VideoSection />
       <ContactForm />
     </div>
   );
